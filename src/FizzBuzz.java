59e768746889c9dcf7ac1381193df2126f41c209
esbaddeley <esbaddeley@gmail.com>
2016-04-14 15:17:21 +0100

    First passing test and one failing test

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,4 +2,8 @@
  * Created by emmabaddeley on 14/04/2016.
  */
 public class FizzBuzz {
+
+    public static String getResults( int number ){
+        if return "Fizz";
+    }
 }
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,8 +1,30 @@
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by emmabaddeley on 14/04/2016.
  */
 public class FizzBuzzTest {
+
+    private String fizz;
+    private String buzz;
+
+    public FizzBuzzTest() {
+        fizz = "Fizz";
+        buzz = "Buzz";
+    }
+
+    @Test
+    public void resultShouldEqualFizzIfMultipleOfThree (){
+        assertEquals(fizz, FizzBuzz.getResults(3));
+    }
+
+    @Test
+    public void resultShouldEqualBuzzIfMultipleOfThree (){
+        assertEquals(buzz, FizzBuzz.getResults(5));
+    }
+
 
 }
