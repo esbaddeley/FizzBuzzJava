eead6af36a5b94a2fdc457f1e6af4b70dd84b5fa
esbaddeley <esbaddeley@gmail.com>
2016-04-14 12:20:16 +0100

    Created FizzBuzz class and Test class files

---
+++ src/FizzBuzz.java
@@ -0,0 +1,5 @@
+/**
+ * Created by emmabaddeley on 14/04/2016.
+ */
+public class FizzBuzz {
+}
---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by emmabaddeley on 14/04/2016.
+ */
+public class FizzBuzzTest {
+
+}
